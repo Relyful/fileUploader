2f06f0307ea0766004dfd9a1332956e9d7d8020a
Relyful <13381270+Relyful@users.noreply.github.com>
2025-07-03 21:58:04 +0200

    Registration now possible

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,71 @@
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient(); // Prisma uses its own pool internally
+
+const registerValidation = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .isAscii()
+    .withMessage("First Name must contain valid ASCII characters."),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords must match!"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password can not be empty.")
+    .isLength({ min: 5 })
+    .withMessage("Password must be atleast 5 characters long."),
+];
+
 exports.getIndex = async (req, res) => {
-  res.render('index');
-}
+  res.render("index");
+};
 
 exports.getRegister = async (req, res) => {
-  res.render('register');
-}
+  res.render("register");
+};
+
+exports.postRegister = [
+  registerValidation,
+  async (req, res, next) => {
+    const data = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).render("register", {
+        errors: errors.errors,
+        loggedIn: req.isAuthenticated(),
+      });
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const role = data.admin ? "ADMIN" : "USER";
+
+    try {
+      let result = await prisma.user.create({
+        data: {
+          username: data.username,
+          password: hashedPassword,
+          role,
+        },
+      });
+      console.log(result);
+      res.redirect("/login");
+    } catch (err) {
+      if (err.code && err.code === "23505") {
+        return res.status(400).render("registerForm", {
+          errors: [{ msg: "Username already exists." }],
+          loggedIn: req.isAuthenticated(),
+          user: req.user,
+        });
+      }
+      return next(err);
+    }
+  },
+];
